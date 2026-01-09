281bb537f9034f6049916e6f4c7a624a8c661472
matteo <matteo.nunziante@gmail.com>
2026-01-09 11:55:48 +0100

    videoprocessor

--- lib_prior/depth_models/depthcrafter/depth_crafter_ppl.py
+++ lib_prior/depth_models/depthcrafter/depth_crafter_ppl.py
@@ -413,11 +413,11 @@ class DepthCrafterPipeline(StableVideoDiffusionPipeline):
                 import logging
                 logging.debug(f"DepthCrafter decoding: {elapsed_time_ms:.0f} ms")
             
-            processor = getattr(self, "video_processor", None)
-            if processor is None:
-                processor = self.image_processor
+            # processor = getattr(self, "video_processor", None)
+            # if processor is None:
+            #     processor = self.image_processor
 
-            frames = self.processor.postprocess_video(
+            frames = self.image_processor.postprocess_video(
                 video=frames, output_type=output_type
             )
         else:
